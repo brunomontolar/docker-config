d72400501f971cb657b15569dc1e14352b67933f
Bruno <brunomontolar@users.noreply.github.com>
2023-01-10 15:03:37 -0300

    Updated Guacamole config

---
+++ guacamole/README.md
@@ -0,0 +1,5 @@
+#Test Initial Setup with variable
+# Run initial setup
+    - Change DOCKERDATA as required, must match DOCKERDATA inside .env
+# Run docker compose
+    - Change variables inside .env as required
---
+++ guacamole/initial_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+DOCKERDATA=~/docker-data/
+
+if ! (docker ps >/dev/null 2>&1)
+then
+        echo "docker daemon not running, will exit here!"
+        exit
+fi
+echo "Preparing folder init and creating ./init/initdb.sql"
+mkdir ./init >/dev/null 2>&1
+chmod -R +x ./init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > =${DOCKERDATA}guacamole/init/initdb.sql
+echo "done"
