98ae0d63cefe68e81bcaa89f63ecaaa7384287ff
Bruno <brunomontolar@users.noreply.github.com>
2023-01-10 17:31:43 -0300

    Fixing Guacamole initial_setup script

--- guacamole/initial_setup.sh
+++ guacamole/initial_setup.sh
@@ -6,8 +6,8 @@ then
         echo "docker daemon not running, will exit here!"
         exit
 fi
-echo "Preparing folder init and creating ./init/initdb.sql"
-mkdir ./init >/dev/null 2>&1
-chmod -R +x ./init
+echo "Preparing folder init and creating {DOCKERDATA}guacamole/init/initdb.sql"
+mkdir ${DOCKERDATA}guacamole/init >/dev/null 2>&1
+chmod -R +x ${DOCKERDATA}guacamole/init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ${DOCKERDATA}guacamole/init/initdb.sql
 echo "done"
