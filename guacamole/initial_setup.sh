596feed472adc5c1931ba564d055933af78d1cbc
Bruno <brunomontolar@users.noreply.github.com>
2023-01-10 17:03:47 -0300

    Fixinig Guacamole initial_setup script

--- guacamole/initial_setup.sh
+++ guacamole/initial_setup.sh
@@ -9,5 +9,5 @@ fi
 echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
 chmod -R +x ./init
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > =${DOCKERDATA}guacamole/init/initdb.sql
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ${DOCKERDATA}guacamole/init/initdb.sql
 echo "done"
